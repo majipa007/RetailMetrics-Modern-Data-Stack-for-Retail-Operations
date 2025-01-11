ba77ac6422a5e8012218c4a392c454cb891d4089
sulav kumar shrestha <sulavstha007@gmail.com>
2025-01-11 09:30:24 +0530

    staging table sources fixed and model description for staging customers added

---
+++ retail_dbt_project/models/staging/staging_customers.sql
@@ -0,0 +1,15 @@
+{{
+    config (materialized = 'table',
+        schema = 'staging_schema',
+        unique_key = 'customer_id'
+                )
+}}
+
+with
+source_data as (
+    select
+        *    from {{ source('retail_shop', 'customer') }}
+)
+select
+    *
+from source_data
